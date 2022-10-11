a8a6ea627411717a8aead6ab0e40ab53389dcc18
Zachary Myre <zmyre@securitybrandsinc.com>
2022-10-11 13:39:36 -0500

    magenda init

---
+++ app.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = 8080;
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "templates")));
+app.use("./templates/images", express.static(__dirname + "/templates/images"));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.sendFile("templates/index.html", { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}`);
+});
