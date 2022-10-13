3ad7f9df5050b697edb567ef0bc814b3a3bf0427
Zachary Myre <zmyre@securitybrandsinc.com>
2022-10-13 11:01:36 -0500

    helpers added.. need to figure out how im going to handle tokens

---
+++ helpers.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const JWT_KEY = process.env.JWT_KEY;
+
+
+function getUserFromToken(token){
+    var user = null;
+    jwt.verify(token, JWT_KEY, (err, decoded) => {
+        if(!err){
+            console.log(decoded);
+            user = decoded;
+        }
+
+      });
+      return decoded.
+}
--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -10,6 +10,7 @@ const { Task, TaskGroup } = require('../database/models');
 */
 router.post('/add/group', async (req, res) => {
     //TODO: grab user_id from the token ? 
+    console.log(req.cookies);
     TaskGroup.create({
         user_id: req.body.user_id,
         name: req.body.name,
@@ -26,7 +27,25 @@ router.post('/add/group', async (req, res) => {
 });
 
 router.post('/add/task', async (req, res) => {
-    return res.status(500).json({message: '', error: true})
+    console.log(req.cookies);
+    console.log(req.cookies == {});
+    Task.create({
+        user_id: req.body.user_id,
+        task_group_id: req.body.task_group_id,
+        description: req.body.description,
+        priority: req.body.priority,
+        completed: req.body.completed ?? false,
+        dueDate: req.body.dueDate ?? '',
+        dateCreated: Date.now()
+    }).then((response) => {
+        if(response){
+            return res.status(200).json({message: "Task created successfully.", error: false, data: response});
+        } else {
+        }
+    }).catch((err) => {
+        console.log(err)
+        return res.status(500).json({message: err, error: true})
+    })
 })
 
 
